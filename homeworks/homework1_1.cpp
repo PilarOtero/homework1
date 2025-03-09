0405699d5322e6fb687858885c6b40fd8804fbd8
Pilar Otero <potero@udesa.edu.ar>
2025-03-08 21:40:22 -0300

    comienzo de la funcion main

--- homeworks/homework1_1.cpp
+++ homeworks/homework1_1.cpp
@@ -18,3 +18,13 @@ vector<vector<int>> generateMtx (int n){
     return mtx;
 }
 
+int main () {
+    int n;
+    cout << "Ingrese el tamaño de la matriz: ";
+    cin >> n;
+
+    while (n < 1){
+        cout << "Ingrese un valor de n valido (debe ser mayor a 1): ";
+        cin >> n;
+    }
+}
