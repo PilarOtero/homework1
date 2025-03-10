43745960537ce90c32925065b97002718d238636
Pilar Otero <potero@udesa.edu.ar>
2025-03-09 21:17:53 -0300

    funcion main

--- homeworks/homework1/punto 2/homework1_2.cpp
+++ homeworks/homework1/punto 2/homework1_2.cpp
@@ -6,13 +6,22 @@ using namespace std;
 
 enum class Severity {DEBUG = 1, INFO = 2, WARNING = 3, ERROR = 4, CRITICAL = 5};
 
-
-void logMessage (string message, Severity severity){
+void logMessage (string message, int severity){
     ofstream file("log.txt", ios::app); 
 
     if (! file){
         cout << "Error al abrir el archivo" << endl;
         return;
     }
-    file << "[" << get_severity(severity) << "]" << message << endl;
+    file << "[" << severity << "]" << "<" << message << ">" << endl;
+}
+
+int main (){
+    logMessage ("Debug message", static_cast<int>(Severity::DEBUG));
+    logMessage ("Info message", static_cast<int>(Severity::INFO));
+    logMessage ("Warning message", static_cast<int>(Severity::WARNING));       
+    logMessage ("Error message", static_cast<int>(Severity::ERROR));
+    logMessage ("Critical message", static_cast<int>(Severity::CRITICAL));
+
+    return 0;
 }
