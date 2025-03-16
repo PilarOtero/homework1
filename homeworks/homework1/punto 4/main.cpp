939347d2a98fc846180723db000bd633f61d9e89
Pilar Otero <potero@udesa.edu.ar>
2025-03-16 12:20:43 -0300

    hice el punto b

--- homeworks/homework1/punto 4/main.cpp
+++ homeworks/homework1/punto 4/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <chrono>
 
 using namespace std;
 
@@ -18,13 +19,12 @@ bool equal_strings(string str1, string str2){
     return false;
 }
 
-/*Uso const char * paara que el compilador pueda evaluar las variables en iempo de compilacion (usando string, al ser de memoria dinamia, 
+/*Uso const char * para que el compilador pueda evaluar las variables en iempo de compilacion (usando string, al ser de memoria dinamia, 
 se evalua en la ejecucion) */
 constexpr int charlength(const char * str){
     if (*str == '\0') return 0;
     return 1 + charlength(str + 1);
 }
-
 
 constexpr bool equal_strings_compilation(const char * char1, const char *char2){
     //CASO BASE: las 2 cadenas estan vacias
@@ -41,12 +41,11 @@ constexpr bool equal_strings_compilation(const char * char1, const char *char2){
 int main(){
     string str1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor nunc quis metus vulputate hendrerit. ";
     string str2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor nunc quis metus vulputate hendrerit. ";
-    string str3 = "Hola mundo";
 
     const char * char1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor nunc quis metus vulputate hendrerit. ";
     const char * char2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor nunc quis metus vulputate hendrerit. ";
-    const char * char3 = "Hola mundo";
     
+    cout << "VERIFICACION EN TIEMPO DE EJECUCION\n" << endl;
     cout << "string1: " << str1 << "\n" << "string2: " << str2 << endl;
     if (equal_strings(str1, str2)){
         cout << "Son iguales" << endl;
@@ -55,26 +54,21 @@ int main(){
         cout << "No son iguales" << endl;
     }
 
-    cout << "\nstring1: " << str1 << "\n" << "string3: " << str3 << endl;
-    if (equal_strings(str1, str3)){
-        cout << "\nSon iguales" << endl;
-    }
-    else {
-        cout << "No son iguales" << endl;
-    }
+    //USANDO CHRONO
+    cout << "\n-UTILIZANDO LA LIBRERIA CHRONO" << endl;
+    auto startTime1 = chrono::high_resolution_clock::now();
+
+    auto endTime1 = chrono::high_resolution_clock::now();
+    auto elapsedTime1 = chrono::duration_cast<std::chrono::nanoseconds>(
+    endTime1 - startTime1);
+    
+    cout << "Ver si los 2 strings eran iguales tomó: "<< elapsedTime1.count() << " nanosegundos" << endl;
+
 
     //VERIFICACION EN TIEMPO DE COMPILACION
     cout << "\nVERIFICACION EN TIEMPO DE COMPILACION\n" << endl;
-    cout << "char1: " << char1 << "\n" << "char3: " << char3 << endl;
+    cout << "char1: " << char1 << "\n" << "char2: " << char2 << endl;
     
-    if (equal_strings_compilation(char1, char3)){
-        cout << "Son iguales" << endl;
-    }
-    else {
-        cout << "No son iguales" << endl;
-    }
-
-    cout << "\nchar1: " << char1 << "\n" << "char2: " << char2 << endl;
     if (equal_strings_compilation(char1, char2)){
         cout << "Son iguales" << endl;
     }
@@ -82,6 +76,13 @@ int main(){
         cout << "No son iguales" << endl;
     }
 
-    return 0;
+    //USANDO CRHONO
+    cout << "\n-UTILIZANDO LA LIBRERIA CHRONO" << endl;
+    auto startTime2 = chrono::high_resolution_clock::now();
+    auto endTime2 = chrono::high_resolution_clock::now();
+    auto elapsedTime2 = chrono::duration_cast<std::chrono::nanoseconds>(
+    endTime2 - startTime2);
+    
+    cout << "Ver si los 2 chars eran iguales tomó: "<< elapsedTime2.count() << " nanosegundos" << endl;
 }
 
